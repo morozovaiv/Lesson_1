3f34f6725398e80bd770f87b825a85bc1b6d18a2
morozovaiv <64019610+morozovaiv@users.noreply.github.com>
2020-04-20 18:56:23 +0300

    Les_1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        int a = 4, b = 3;
+        int c = a + b; ;
+        Console.WriteLine(c);
+    }
+}
